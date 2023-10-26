57445fde1157bed2c507534325e8280032d65684
BrMaro <keremamariari@gmail.com>
2023-10-26 08:07:48 +0300

    fixed minor errors

--- main.py
+++ main.py
@@ -12,27 +12,29 @@ import os
 #Set up Google Chrome option and open browser
 options = Options()
 options.add_experimental_option("detach", True)
+options.add_argument("user-data-dir=C:\\Users\\Techron\\AppData\\Local\\Google\\Chrome\\User Data")
 driver = webdriver.Chrome(options=options)
 url = "https://store.epicgames.com/en-US/"
 driver.get(url)
 driver.implicitly_wait(10)
 
-free_games_div = driver.find_element(By.CLASS_NAME,"css-1vu10h2")
-free_games = free_games_div.find_elements(By.CLASS_NAME,"css-1h2ruwl")
-print([x.text for x in free_games])
 
-print("t3")
-status = free_games_div.find_elements(By.CLASS_NAME,"css-11xvn05")
-driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", free_games_div)
 time.sleep(5)
-print("t4")
-for game in range(len(status)):
 
-    print(free_games[game].text)
-    print("t5")
+game = 0
+while True:
+    free_games_div = driver.find_element(By.CLASS_NAME, "css-1vu10h2")
+    free_games = free_games_div.find_elements(By.CLASS_NAME, "css-1h2ruwl")
+    status = free_games_div.find_elements(By.CLASS_NAME, "css-11xvn05")
+    driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", free_games_div)
+
+    if game == len(status):
+        break
+
+    name = free_games[game].text
+    print(name)
     free_games[game].click()
     driver.implicitly_wait(5)
-    print("t6")
     try:
         age_block = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, "css-quioav")))
         continue_button = driver.find_element(By.CLASS_NAME, "css-1a6we1t")
@@ -41,17 +43,25 @@ for game in range(len(status)):
         pass
 
     driver.implicitly_wait(5)
-    get_button = driver.find_element(By.XPATH,"(//div[@class='css-uwwqev'])[3]")
 
-    if get_button.is_enabled():
+    try:
+        get_button = WebDriverWait(driver,3).until(EC.presence_of_element_located((By.XPATH,"(//button[@class='css-1vkt3xd'])")))
+    except selenium.common.TimeoutException:
+        get_button = driver.find_element(By.XPATH,"//span[@class = 'css-8en90x']")
+
+
+    if get_button.text != "IN LIBRARY":
         get_button.click()
         place_order_button = driver.find_element(By.XPATH,"//button[contains(@class,'payment-btn payment-order-confirm__btn')]")
         place_order_button.click()
         driver.implicitly_wait(10)
-        print(f"{free_games[game].text} saved to the library")
+        print(f"{name} saved to the library")
     else:
-        print(f"{free_games[game].text} in library already")
+        print(f"{name} in library already")
+        driver.back()
+
+    game += 1
 
 
-
+print("Exiting Browser")
 driver.quit()
